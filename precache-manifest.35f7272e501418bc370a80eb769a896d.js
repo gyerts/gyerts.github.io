0db733f69e25d4422c4879add4546ced367e58f4
Yuriy Gyerts <gyerts@gmail.com>
2018-10-28 17:32:19 +0200

    Updates

---
+++ precache-manifest.35f7272e501418bc370a80eb769a896d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "269395bb9971a0cf0e3a52d4beeb1f1d",
+    "url": "/holylogger/static/media/showen.269395bb.png"
+  },
+  {
+    "revision": "08468bc8413f0a1ce332426ee2579d96",
+    "url": "/holylogger/static/media/hidden.08468bc8.png"
+  },
+  {
+    "revision": "4d926be20eb9bb8e47a0",
+    "url": "/holylogger/static/js/runtime~main.4d926be2.js"
+  },
+  {
+    "revision": "b9b41018229c88ebf889",
+    "url": "/holylogger/static/js/main.b9b41018.chunk.js"
+  },
+  {
+    "revision": "3e00e7c6ea7ed3e307ba",
+    "url": "/holylogger/static/js/1.3e00e7c6.chunk.js"
+  },
+  {
+    "revision": "b9b41018229c88ebf889",
+    "url": "/holylogger/static/css/main.fa95e496.chunk.css"
+  },
+  {
+    "revision": "2d4c8cf195c3ccebfaa13ea214afb03b",
+    "url": "/holylogger/index.html"
+  }
+];
